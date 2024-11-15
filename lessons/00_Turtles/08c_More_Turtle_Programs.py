334b7e4c0204d9755b3d5560360b6fb901ab24a6
weridethebus <christopher.aguilar@stmartinoftoursacademy.org>
2024-11-15 20:33:21 +0000

    germ

--- lessons/00_Turtles/08c_More_Turtle_Programs.py
+++ lessons/00_Turtles/08c_More_Turtle_Programs.py
@@ -1,12 +1,14 @@
-
-
 import turtle
-turtle.setup(510, 250)
-
-
-
-
-
+turtle.setup(525, 250, startx = 0, starty = 0)
+tina = turtle.Turtle()
+tina.speed(1000000000000000000000000000000000000000000)
 
 def circle_fractal():
-    
+    for i in range(10000000000000000000000000000000000000000000000000000000000000000000):
+        print("loop iteration", i)
+        tina.pencolor("green")
+        tina.circle(i*0.9999999999999999999999999999999999999999999999999999999999999999999999999999999)
+
+circle_fractal()
+
+turtle.exitonclick()
