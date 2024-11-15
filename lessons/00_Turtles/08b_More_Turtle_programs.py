10bfce647d119a6b6a90154eb0b4e042f7e5ac78
weridethebus <christopher.aguilar@stmartinoftoursacademy.org>
2024-11-15 20:33:11 +0000

    skibde

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -7,26 +7,28 @@ from the section "Change the Turtle Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
-
 import turtle
-turtle.setup(width=500, height=500, starx=0, starty=0)
+
 
 def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
     from pathlib import Path
-    image_dir = Path(_file_).parent / "images"
+    image_dir = Path(__file__).parent / "images"
     image_path = str(image_dir / image_name)
 
     screen = turtle.getscreen()
     screen.addshape(image_path)
     turtle.shape(image_path)
 
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=530, height=365, startx = 0, starty = 0)
 
-screen = turtle.screen()
-screen.setup(width = 600, height = 600)
-
+# Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t, "emoji.png")
+set_turtle_image(t, "boy_yellow.gif")
 
 t.penup()
 t.speed(3)
@@ -34,3 +36,6 @@ t.speed(3)
 for i in range(4):
     t.goto(200, 200)
     t.goto(-200, -200)
+
+
+turtle.exitonclick()     
