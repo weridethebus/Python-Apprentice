aa2ecae99455c398a798248baa26233dbdf6b6f0
weridethebus <christopher.aguilar@stmartinoftoursacademy.org>
2024-11-22 04:46:23 +0000

    they know me

--- lessons/00_Turtles/14_Crazy_Spiral.py
+++ lessons/00_Turtles/14_Crazy_Spiral.py
@@ -3,14 +3,16 @@ turtle.setup( width = 1450, height = 700, startx = 0, starty = 0)
 rootbeer = turtle.Turtle()
 rootbeer.speed(0)
 
-sides = 3
+rootbeer.pencolor("blue")
+
+sides = 4
 angle = 360/sides
 
 def crazy_spiral():
-    for i in range(360):
+    for i in range(500):
         print("loop iteration", i)
-        rootbeer.forward (i*1.00000000001)
-        rootbeer.left(angle + 0.0625)
+        rootbeer.forward (i*1.00000001)
+        rootbeer.left(angle + 0)
 
 
 crazy_spiral()
