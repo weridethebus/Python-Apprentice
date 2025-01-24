a3511232a093f5c6461a4673f01ae734640dc4bf
weridethebus <christopher.aguilar@stmartinoftoursacademy.org>
2025-01-24 21:56:51 +0000

    g

--- lessons/00_Turtles/04b_Turtle_Tricks.py
+++ lessons/00_Turtles/04b_Turtle_Tricks.py
@@ -2,18 +2,21 @@
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600, starty = 0, startx = 0)    # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
-tina.speed = 0
+tina.set_speed = 100
 
 def shape_spiral():
     for i in range(500):
         tina.forward(i*1.001)
         tina.left(72 * 1.01)
+for i in range(500):
         tina.forward(i*1.001)
-        tina.left(120 * 1.01)
+        tina.left(72 * 1.01)
+for i in range(500):
         tina.forward(i*1.001)
-        tina.left(90 * 1.01)
+        tina.left(72 * 1.01)
+for i in range(500):
         tina.forward(i*1.001)
-        tina.left(60 * 1.01)
+        tina.left(72 * 1.01)
 shape_spiral()
 
 
