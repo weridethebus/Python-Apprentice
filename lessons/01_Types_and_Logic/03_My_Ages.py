41b785c099708b596b51aedc8b43dbd0a1a2ef68
weridethebus <christopher.aguilar@stmartinoftoursacademy.org>
2025-02-15 06:01:50 +0000

    Ihateplayerswhojustspamminigunoransmgandbuildalloveryouandgetyoulost

--- lessons/01_Types_and_Logic/03_My_Ages.py
+++ lessons/01_Types_and_Logic/03_My_Ages.py
@@ -8,8 +8,8 @@ tell the user what age groups the user is in. The groups are:
 0-2: Baby
 3-5: Toddler
 6-12: Child
-13-19: Teen
-20-64: Adult
+13-17: Teen
+18-64: Adult
 65+: Senior
 
 Except, if the user is the same age as you, print "You are pretty awesome!"
@@ -22,6 +22,7 @@ age = simpledialog.askinteger("Your Age", "How old are you?")
 Or, you could ask the user for a float with simpledialog.askfloat() 
 
 age =  simpledialog.askfloat("Your Age", "How old are you?")
+
 
 
 Here is how you show the user a message window. The first parameter is the title of the window, 
@@ -42,11 +43,30 @@ window.withdraw() # Hide the window; we just want to see pop ups
 # and create a message
 
 # Show the message to the user
+age =  simpledialog.askinteger("Your Age", "How old are you?")
+
+if age >= 0 and age <= 2:
+    print(" You are a baby.")
+elif age >= 3 and age <= 5:
+    print("You are a toddler.")
+elif age >= 6 and age <= 10:
+    print(" You are a child.")
+elif age >= 11 and age <= 12:
+    print("You are a  pre-teen.")
+elif age >= 13 and age <= 17:
+    print("You are a teen.")
+elif age >= 18 and age <= 64:
+    print(" You are an adult.")
+else:
+    print("You are a senior.")
 
 
 
-window.mainloop()  # Keeps the window open
+
+#window.mainloop()  # Keeps the window open
 
 
 # TODO: 
 # Try to write your program so you only need to use one messagebox.showinfo() function.
+
+
